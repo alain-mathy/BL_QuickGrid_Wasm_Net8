1aeb5b21439a9c7f25eb4ccfd6c51e5cf7927b60
alain-mathy <alain_mathy@hotmail.com>
2025-08-25 16:46:46 +0200

    Add GetAllCustomers -> CustomerController

---
+++ BL_QuickGrid_Wasm_Net8/BL_QuickGrid_Wasm_Net8/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using BL_QuickGrid_Wasm_Net8.Data;
+using BL_QuickGrid_Wasm_Net8.Shared.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BL_QuickGrid_Wasm_Net8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetAllCustomers()
+        {
+            var result = await _context.Customers.ToListAsync();
+            return Ok(result);
+        }
+    }
+}
